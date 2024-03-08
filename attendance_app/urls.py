9e20943f86dd7216c58caf1a52f9adc8b5f1a0be
coffee-jinn-2002 <jinn140515.2002@gmail.com>
2024-03-09 00:29:13 +0900

    add adminpage,userpage and add LoginView def

--- attendance_app/urls.py
+++ attendance_app/urls.py
@@ -6,7 +6,16 @@ app_name = "attendance_app"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path('signup/', views.SignupView.as_view(), name="signup"),
-    path('login/', views.LoginView.as_view(), name="login"),
-    path('logout/', views.LogoutView.as_view(), name="logout"),
+    path("signup/", views.SignupView.as_view(), name="signup"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("ad/dashboard/", views.AdminDashboardView.as_view(), name="admin_dashboard"), 
+    path("user/dashboard/", views.UserDashboardView.as_view(), name="user_dashboard"),
 ]
+
+# urlpatterns = [
+#     path("", views.IndexView.as_view(), name="index"),
+#     path('signup/', views.SignupView.as_view(), name="signup"),
+#     path('login/', views.LoginView.as_view(), name="login"),
+#     path('logout/', views.LogoutView.as_view(), name="logout"),
+# ]
--- attendance_app/views.py
+++ attendance_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login, authenticate
 from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.views import LoginView as BaseLoginView,  LogoutView as BaseLogoutView
@@ -28,11 +31,28 @@ class SignupView(CreateView):
         login(self.request, user)
         return response
     
-# ログインビューを作成
 class LoginView(BaseLoginView):
     form_class = LoginFrom
     template_name = "attendance_app/login.html"
 
+    def form_valid(self, form):
+        # 親クラスのform_validを呼び出してログインを処理
+        response = super().form_valid(form)
+        # ログインしたユーザーを取得
+        user = form.get_user()
+        # is_adminの値によってリダイレクト先を設定
+        if user.is_authenticated and user.is_admin:
+            return HttpResponseRedirect(reverse('attendance_app:admin_dashboard'))
+        else:
+            return HttpResponseRedirect(reverse('attendance_app:user_dashboard'))
+
 # LogoutViewを追加
 class LogoutView(BaseLogoutView):
     success_url = reverse_lazy("attendance_app:index")
+
+class AdminDashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'attendance_app/admindash.html'
+
+
+class UserDashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'attendance_app/userdash.html'
